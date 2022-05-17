da8b53c527d9b6e617fc0203d8175523f952541f
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-05-17 22:00:02 +0900

    view Binding 사용 / HomeFragment 기본 구성

--- app/src/main/java/com/duran/usedtradeapp/HomeFragment.kt
+++ app/src/main/java/com/duran/usedtradeapp/HomeFragment.kt
@@ -1,7 +1,21 @@
 package com.duran.usedtradeapp
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.duran.usedtradeapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var binding : FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("sslee", "onViewCreated")
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+    }
 
 }
